52c4a11867c9dccde5e52e498f9594836649fc83
Travis Eden <daovist@gmail.com>
2018-09-26 12:00:42 -0400

    clear publish and edit pages based on publish.isUpdate

--- client/src/pages/HomePage/view.jsx
+++ client/src/pages/HomePage/view.jsx
@@ -4,8 +4,10 @@ import PublishTool from '@containers/PublishTool';
 import ContentPageWrapper from '@pages/ContentPageWrapper';
 
 class HomePage extends React.Component {
-  componentWillUnmount () {
-    this.props.clearFile();
+  componentDidMount () {
+    if (this.props.isUpdate) {
+      this.props.clearFile();
+    }
   }
   render () {
     const { homeChannel } = this.props;
